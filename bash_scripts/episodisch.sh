d8d86902172682f40a16f749de41b721e0427dea
ChatGPT-Developer-42 <benedict.kunzmann@st.ovgu.de>
2024-01-22 09:51:57 +0100

    small changes

--- bash_scripts/episodisch.sh
+++ bash_scripts/episodisch.sh
@@ -10,4 +10,4 @@
 #SBATCH --mem-per-cpu=4G
 
 #SBATCH --array=1-10
-CUDA_VISIBLE_DEVICES="" python3 main.py --name 'mlp-episodisch-100-fixedScaling-SLR0025_fixedDF' --learning_rate 0.005 --decay_rate 0.80 --n_layers 11 --distance 750 --batch_size 1024 --epochs 2880 --regularization True --regularization_value 0.00000001 --dropout True --dropout_value 0.01 --activation_final "tanh"
+CUDA_VISIBLE_DEVICES="" python3 main.py --name 'mlp-episodisch-100-fixedScaling-SLR0025_Neu900' --learning_rate 0.005 --n_neurons 900 --decay_rate 0.80 --n_layers 11 --distance 750 --batch_size 1024 --epochs 2880 --regularization True --regularization_value 0.00000001 --dropout True --dropout_value 0.01 --activation_final "tanh"
