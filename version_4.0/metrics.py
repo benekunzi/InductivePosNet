560c1e036b8bbae17dde7ac29ba5fdb6fa93453c
ChatGPT-Developer-42 <benedict.kunzmann@st.ovgu.de>
2024-02-05 09:34:22 +0100

    bug fixes

--- version_4.0/metrics.py
+++ version_4.0/metrics.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+distance_center = 200
 
 class MeanXYSquarredErrorRealWorld(tf.keras.metrics.MeanSquaredError):
     def __init__(self, scaler, *args, **kwargs):
@@ -7,7 +10,12 @@ class MeanXYSquarredErrorRealWorld(tf.keras.metrics.MeanSquaredError):
 
     def inverse_transform(self, y):
         # original sklearn
-        return (y - self.scaler.min_) / self.scaler.scale_
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
 
     def update_state(self, y_pred, y_target, *args, **kwargs):
         super().update_state(self.inverse_transform(y_pred), 
@@ -20,7 +28,12 @@ class MeanXYAbsoluteErrorRealWorld(tf.keras.metrics.MeanAbsoluteError):
 
     def inverse_transform(self, y):
         # original sklearn
-        return (y - self.scaler.min_) / self.scaler.scale_
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
 
     def update_state(self, y_pred, y_target, *args, **kwargs):
         super().update_state(self.inverse_transform(y_pred), 
@@ -30,10 +43,15 @@ class MeanXYRootSquarredErrorRealWorld(tf.keras.metrics.RootMeanSquaredError):
     def __init__(self, scaler, *args, **kwargs):
         super().__init__(name="rmseXYReal", *args, **kwargs)
         self.scaler = scaler
-
+        
     def inverse_transform(self, y):
         # original sklearn
-        return (y - self.scaler.min_) / self.scaler.scale_
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
 
     def update_state(self, y_pred, y_target, *args, **kwargs):
         super().update_state(self.inverse_transform(y_pred), 
@@ -46,7 +64,12 @@ class MeanXYNormRealWorld(tf.keras.metrics.Mean):
 
     def inverse_transform(self, y):
         # original sklearn
-        return (y - self.scaler.min_) / self.scaler.scale_
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
 
     def update_state(self, y_pred, y_target, *args, **kwargs):
 
@@ -64,7 +87,12 @@ class CenterMeanXYNormRealWorld(tf.keras.metrics.Mean):
 
     def inverse_transform(self, y):
         # original sklearn
-        return (y - self.scaler.min_) / self.scaler.scale_
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
 
     def update_state(self, y_pred, y_target, *args, **kwargs):
 
@@ -74,6 +102,70 @@ class CenterMeanXYNormRealWorld(tf.keras.metrics.Mean):
         norm = tf.norm(error, axis=1)
 
         distance = tf.norm(y_target, axis=1)
-        selection = tf.boolean_mask(norm, distance < 50)
+        selection = tf.boolean_mask(norm, distance < distance_center)
 
         super().update_state(selection, *args, **kwargs)
+
+
+class CenterMaxXYNormRealWorld(tf.keras.metrics.Metric):
+    def __init__(self, scaler, *args, **kwargs):
+        super().__init__(name="CenterMaxNormXYReal", *args, **kwargs)
+        self.scaler = scaler
+        self.max = self.add_weight("max", initializer=tf.keras.initializers.Zeros(), dtype=tf.float32)
+
+    def inverse_transform(self, y):
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
+
+    def update_state(self, y_pred, y_target, *args, **kwargs):
+
+        y_pred = self.inverse_transform(y_pred)
+        y_target = self.inverse_transform(y_target)
+        error = y_target - y_pred
+        norm = tf.norm(error, axis=1) 
+
+        distance = tf.norm(y_target, axis=1)
+        selection = tf.boolean_mask(norm, distance < distance_center)
+
+        norm_max = tf.math.reduce_max(selection)
+
+        if norm_max > self.max:
+            self.max.assign(norm_max)
+
+    def result(self):
+        return self.max
+
+    def reset_state(self):
+        self.max.assign(0.0)
+
+class CenterWithinTresholdXYNormRealWorld(tf.keras.metrics.Mean):
+    def __init__(self, scaler, *args, **kwargs):
+        super().__init__(name="CenterWithinTresholdNormXYReal", *args, **kwargs)
+        self.scaler = scaler
+
+    def inverse_transform(self, y):
+        # original sklearn
+        if type(self.scaler) == MinMaxScaler: 
+            return (y - self.scaler.min_) / self.scaler.scale_
+        elif type(self.scaler) == StandardScaler: 
+            y *= self.scaler.scale_
+            # y *= self.scaler.mean_
+            return y
+
+    def update_state(self, y_pred, y_target, *args, **kwargs):
+
+        y_pred = self.inverse_transform(y_pred)
+        y_target = self.inverse_transform(y_target)
+        error = y_target - y_pred
+        norm = tf.norm(error, axis=1)
+
+        distance = tf.norm(y_target, axis=1)
+        selection = tf.boolean_mask(norm, distance < distance_center)
+
+        percentage = (tf.math.reduce_mean(tf.cast(selection < 25, tf.float32))) * 100
+        
+        super().update_state(percentage, *args, **kwargs)
